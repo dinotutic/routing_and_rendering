948dd42a64e701cf58183fc2263797f8df955a70
Dino Tutic <dino.tutic96@gmail.com>
2024-05-25 02:13:50 +0200

    Added dummy articles and improved /news and /news/newsSlug page

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <div id="error">
+      <h1>Page not found!</h1>
+      <p>The requested resource could not be found!</p>
+    </div>
+  );
+}
