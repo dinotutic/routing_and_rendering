73281ddca89380d6a86ed443742d12da61e1365e
Dino Tutic <dino.tutic96@gmail.com>
2024-05-27 22:33:39 +0200

    Added catchall routes for filtering year/month

--- app/archive/@archive/[yearSlug]/page.js
+++
@@ -1,7 +0,0 @@
-import { getNewsForYear } from "@/lib/news";
-import NewsList from "@/components/news-list";
-export default function FilteredNewsPage({ params }) {
-  const newsYear = params.yearSlug;
-  const news = getNewsForYear(newsYear);
-  return <NewsList news={news} />;
-}
